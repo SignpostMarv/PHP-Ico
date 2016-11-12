76a4518fae5eff80f99c6b036398fed0360a4b63
SignpostMarv <me@signpostmarv.name>
2016-11-12 21:14:47 +0000

    porting in changes from old-master (re: chrisbliss18/php-ico#5)

--- class-php-ico.php
+++ class-php-ico.php
@@ -7,6 +7,9 @@ Licensed under GPLv2 or above
 Version 1.0.4
 */
 
+use Intervention\Image\Image;
+use Intervention\Image\ImageManagerStatic;
+
 class PHP_ICO
 {
     /**
@@ -15,14 +18,6 @@ class PHP_ICO
      * @var array
      */
     private $_images = array();
-
-    /**
-     * Flag to tell if the required functions exist.
-     *
-     * @var boolean
-     */
-    private static $_has_requirements = null;
-
 
     /**
      * Constructor - Create a new ICO generator.
@@ -35,37 +30,6 @@ class PHP_ICO
      */
     public function __construct($file = false, $sizes = array())
     {
-        if (is_null(self::$_has_requirements)) {
-            $required_functions = array(
-                'getimagesize',
-                'imagecreatefromstring',
-                'imagecreatetruecolor',
-                'imagecolortransparent',
-                'imagecolorallocatealpha',
-                'imagealphablending',
-                'imagesavealpha',
-                'imagesx',
-                'imagesy',
-                'imagecopyresampled',
-            );
-
-            foreach ($required_functions as $function) {
-                if (! function_exists($function)) {
-                    self::$_has_requirements = false;
-                    break;
-                    return;
-                }
-            }
-
-            if (is_null(self::$_has_requirements)) {
-                self::$_has_requirements = true;
-            }
-        }
-        if (self::$_has_requirements === false) {
-            throw new \RuntimeException("The PHP_ICO class was unable to find the $function function, which is part of the GD library. Ensure that the system has the GD library installed and that PHP has access to it through a PHP interface, such as PHP's GD module. Since this function was not found, the library will be unable to create ICO files.");
-        }
-
-
         if (false != $file) {
             $this->add_image($file, $sizes);
         }
@@ -85,35 +49,43 @@ class PHP_ICO
      */
     public function add_image($file, $sizes = array())
     {
-        $im = $this->_load_image_file($file);
+        $im = ImageManagerStatic::make($file);
 
 
         if (empty($sizes)) {
-            $sizes = array( imagesx($im), imagesy($im) );
+            $sizes = array( $im->width(), $im->height() );
         }
 
         // If just a single size was passed, put it in array.
-        if (! is_array($sizes[0])) {
-            $sizes = array( $sizes );
-        }
+        $sizes = array_values(
+            array_reduce(
+                array_map(
+                    function ($e) {
+                        if (!is_array($e)) {
+                            $e = array($e);
+                        }
+                        if (count($e) === 1) {
+                            $e = array(current($e), current($e));
+                        }
+                        $e = array_values($e);
+                        return array($e[0], $e[1]);
+                    },
+                    (array)$sizes
+                ),
+                function ($was, $size) {
+                    list($width, $height) = $size;
+                    $was[$width . ':' . $height] = $size;
+                    return $was;
+                }
+            )
+        );
 
-        foreach ((array) $sizes as $size) {
+        foreach ($sizes as $size) {
             list($width, $height) = $size;
-
-            $new_im = imagecreatetruecolor($width, $height);
-
-            imagecolortransparent($new_im, imagecolorallocatealpha($new_im, 0, 0, 0, 127));
-            imagealphablending($new_im, false);
-            imagesavealpha($new_im, true);
-
-            $source_width = imagesx($im);
-            $source_height = imagesy($im);
-
-            if (false === imagecopyresampled($new_im, $im, 0, 0, 0, 0, $width, $height, $source_width, $source_height)) {
-                continue;
-            }
-
+            $im->backup();
+            $new_im = $im->resize($width, $height);
             $this->_add_image_data($new_im);
+            $im->reset();
         }
 
         return true;
@@ -159,136 +131,100 @@ class PHP_ICO
                 '() with no images!'
             );
         }
-
-
         $data = pack('vvv', 0, 1, count($this->_images));
-        $pixel_data = '';
-
-        $icon_dir_entry_size = 16;
-
-        $offset = 6 + ($icon_dir_entry_size * count($this->_images));
+        $pixelData = '';
+        $iconDirEntrySize = 16;
+        $offset = 6 + ($iconDirEntrySize * (count($this->_images)));
 
         foreach ($this->_images as $image) {
-            $data .= pack('CCCCvvVV', $image['width'], $image['height'], $image['color_palette_colors'], 0, 1, $image['bits_per_pixel'], $image['size'], $offset);
-            $pixel_data .= $image['data'];
-
+            $data .= pack(
+                'CCCCvvVV',
+                $image['width'],
+                $image['height'],
+                $image['color_palette_colors'],
+                0,
+                1,
+                $image['bits_per_pixel'],
+                $image['size'],
+                $offset
+            );
+            $pixelData .= $image['data'];
             $offset += $image['size'];
         }
 
-        $data .= $pixel_data;
-        unset($pixel_data);
-
+        $data .= $pixelData;
+        unset($pixelData);
 
         return $data;
     }
 
     /**
-     * Take a GD image resource and change it into a raw BMP format.
+     * Take a image resource and change it into a raw BMP format.
      */
-    private function _add_image_data($im)
+    private function _add_image_data(Image $im)
     {
-        $width = imagesx($im);
-        $height = imagesy($im);
+        $width = $im->width();
+        $height = $im->height();
+
+        $pixelData = array();
+        $opacityData = array();
+        $currentOpacityVal = 0;
+
+        for ($y=($height - 1);$y>=0;$y--) {
+            for ($x=0;$x<$width;$x++) {
+                $color = $im->pickColor($x, $y, 'integer');
+
+                list($r, $g, $b, $alpha) = $im->pickColor($x, $y, 'array');
+
+                $opacity = ($alpha <= 0.5) ? 1 : 0;
+
+                $alpha *= 255;
+                $alpha |= 0;
 
 
-        $pixel_data = array();
+                $color &= 0xffffff;
+                $color |= 0xff000000 & ($alpha << 24);
 
-        $opacity_data = array();
-        $current_opacity_val = 0;
+                $pixelData[] = $color;
 
-        for ($y = $height - 1; $y >= 0; $y--) {
-            for ($x = 0; $x < $width; $x++) {
-                $color = imagecolorat($im, $x, $y);
-
-                $alpha = ($color & 0x7F000000) >> 24;
-                $alpha = (1 - ($alpha / 127)) * 255;
-
-                $color &= 0xFFFFFF;
-                $color |= 0xFF000000 & ($alpha << 24);
-
-                $pixel_data[] = $color;
-
-
-                $opacity = ($alpha <= 127) ? 1 : 0;
-
-                $current_opacity_val = ($current_opacity_val << 1) | $opacity;
+                $currentOpacityVal = ($currentOpacityVal << 1) | $opacity;
 
                 if ((($x + 1) % 32) == 0) {
-                    $opacity_data[] = $current_opacity_val;
-                    $current_opacity_val = 0;
+                    $opacityData[] = $currentOpacityVal;
+                    $currentOpacityVal = 0;
                 }
             }
-
             if (($x % 32) > 0) {
                 while (($x++ % 32) > 0) {
-                    $current_opacity_val = $current_opacity_val << 1;
+                    $currentOpacityVal = $currentOpacityVal << 1;
                 }
 
-                $opacity_data[] = $current_opacity_val;
-                $current_opacity_val = 0;
+                $opacityData[] = $currentOpacityVal;
+                $currentOpacityVal = 0;
             }
         }
 
-        $image_header_size = 40;
-        $color_mask_size = $width * $height * 4;
-        $opacity_mask_size = (ceil($width / 32) * 4) * $height;
+        $imageHeaderSize = 40;
+        $colorMaskSize = $width * $height * 4;
+        $opacityMaskSize = (ceil($width / 32) * 4) * $height;
 
-
-        $data = pack('VVVvvVVVVVV', 40, $width, ($height * 2), 1, 32, 0, 0, 0, 0, 0, 0);
-
-        foreach ($pixel_data as $color) {
+        $data =
+            pack('VVVvvVVVVVV', 40, $width, ($height * 2), 1, 32, 0, 0, 0, 0, 0, 0)
+        ;
+        foreach ($pixelData as $color) {
             $data .= pack('V', $color);
         }
-
-        foreach ($opacity_data as $opacity) {
+        foreach ($opacityData as $opacity) {
             $data .= pack('N', $opacity);
         }
 
-
-        $image = array(
-            'width'                => $width,
-            'height'               => $height,
+        $this->_images[] = array(
+            'width' => $width,
+            'height' => $height,
             'color_palette_colors' => 0,
-            'bits_per_pixel'       => 32,
-            'size'                 => $image_header_size + $color_mask_size + $opacity_mask_size,
-            'data'                 => $data,
+            'bits_per_pixel' => 32,
+            'size' => ($imageHeaderSize + $colorMaskSize + $opacityMaskSize),
+            'data' => $data,
         );
-
-        $this->_images[] = $image;
-    }
-
-    /**
-     * Read in the source image file and convert it into a GD image resource.
-     */
-    private function _load_image_file($file)
-    {
-        if (!is_string($file) || empty($file)) {
-            throw new \InvalidArgumentException(
-                'File not specified!'
-            );
-        }
-        // Run a cheap check to verify that it is an image file.
-        if (false === ($size = getimagesize($file))) {
-            throw new \InvalidArgumentException(
-                'Could not determine image size!'
-            );
-        }
-
-        if (false === ($file_data = file_get_contents($file))) {
-            throw new \InvalidArgumentException(
-                'Could not open file!'
-            );
-        }
-
-        if (false === ($im = imagecreatefromstring($file_data))) {
-            throw new \InvalidArgumentException(
-                'Could not parse file!'
-            );
-        }
-
-        unset($file_data);
-
-
-        return $im;
     }
 }
